b909c06442fddb37e31b60b393ad9a7ea9c5956e
Steve Siegel <stevegabrielsiegel@gmail.com>
2021-07-22 21:38:43 -0500

    finished the add-post and comment files in the public folder

---
+++ public/javascript/add-post.js
@@ -0,0 +1,31 @@
+const { response } = require("express");
+
+async function newFormHandler(event) {
+  event.preventDefault();
+
+  const title = document.querySelector('input[name="post-title"]').value;
+  const post_content = document
+    .querySelector('textarea[name="post-content"]')
+    .value.trim();
+
+  const respone = await fetch("/api/posts", {
+    method: "POST",
+    body: JSON.stringify({
+      title,
+      post_content,
+    }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  if (response.ok) {
+    document.location.replace("/dashboard");
+  } else {
+    alert(response.statusText);
+  }
+}
+
+document
+  .querySelector(".new-post-form")
+  .addEventListener("submit", newFormHandler);
